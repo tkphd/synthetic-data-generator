dcd0a29ae31830d828b72c87b97d9dc24031c2ea
Trevor Keller <trevor.keller@nist.gov>
2019-06-20 12:44:03 -0400

    dynamic folder input

--- FS-march.sh
+++ FS-march.sh
@@ -1,48 +1,56 @@
-#!/usr/bin/env bash
+ #!/usr/bin/env bash
 
 # Sweep over dataset and file sizes to test BeeGFS scaling:
 # test datasets in the range of 10 MB to 1 GB
 # with mean filesize in the range of 10 KB to 10 MB
 
-# Path variables
-DIR=~/research/projects/BeeGFS/synthetic-dataset-generator
-BIN=${DIR}/./data-generator     # absolute path to binary file
-TYM=$(date +%s)                 # UNIX epoch timestamp
-LOG="${DIR}/${TYM}_FS-march.log" # absolute path to log file
+if [ $# != 1 ]; then
+    echo "Usage: ${0} output_directory"
+elif [[ ! -d ${1} ]]; then
+    echo "Error: ${1} is not a directory."
+    echo "Usage: ${0} output_directory"
+else
+    DIR=$(pwd)
+    BIN=${DIR}/./data-generator     # absolute path to binary file
+    TYM=$(date +%s)                 # UNIX epoch timestamp
+    LOG="${DIR}/${TYM}_FS-march.log" # absolute path to log file
 
-# CLI options
-DM=1000000                      # data-size multiplier
-RP=3                            # number of repetitions
+    cd $1
 
-# Real Values
-LGA=( 1 2 4 6 8 )
-NFA=( 10 100 1000 10000 )
+    # CLI options
+    DM=1000000                      # data-size multiplier
+    RP=3                            # number of repetitions
 
-# Total number of jobs
-LGN=${#LGA[@]}
-NFN=${#NFA[@]}
-NJ=$(( LGN*NFN ))
+    # Real Values
+    LGA=( 1 2 4 6 8 )
+    NFA=( 10 100 1000 10000 )
 
-# Write log file column headers
-echo "Writing summary to ${LOG}"
-echo -ne "data         file            " > "${LOG}"
-for ((i=0; i<RP; i++))
-do
-    echo -ne "time         " >> "${LOG}"
-done
-echo -e "write        stdev        remove" >> "${LOG}"
+    # Total number of jobs
+    LGN=${#LGA[@]}
+    NFN=${#NFA[@]}
+    NJ=$(( LGN*NFN ))
 
-n=1
-for j in "${NFA[@]}"
-do
-    datasize=$(( j*DM ))
-    for i in "${LGA[@]}"
+    # Write log file column headers
+    echo "Writing summary to ${LOG}"
+    echo -ne "data         file            " > "${LOG}"
+    for ((i=0; i<RP; i++))
     do
-        filesize=$(( datasize/(i*j) ))
-        printf "%2d/${NJ} " ${n}
-        /usr/bin/time -f'     (%E wall, %U user, %S sys)' \
-                      ${BIN} -t ${datasize} -m ${filesize} -r ${RP} >> "${LOG}"
-        n=$(( n+1 ))
+        echo -ne "time         " >> "${LOG}"
     done
-done
-echo "Wrote summary to ${LOG}"
+    echo -e "write        stdev        remove" >> "${LOG}"
+
+    n=1
+    for j in "${NFA[@]}"
+    do
+        datasize=$(( j*DM ))
+        for i in "${LGA[@]}"
+        do
+            filesize=$(( datasize/(i*j) ))
+            printf "%2d/${NJ} " ${n}
+            /usr/bin/time -f'     (%E wall, %U user, %S sys)' \
+                          ${BIN} -t ${datasize} -m ${filesize} -r ${RP} -d >> "${LOG}"
+            n=$(( n+1 ))
+        done
+    done
+    echo "Wrote summary to ${LOG}"
+fi
