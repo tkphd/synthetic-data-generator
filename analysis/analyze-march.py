52e8e318f5e54074c6addc1ae69ed811cdc6a9cf
Trevor Keller <trevor.keller@nist.gov>
2019-06-20 12:14:49 -0400

    dynamic file input, bandwidth plot

--- analysis/analyze-march.py
+++ analysis/analyze-march.py
@@ -1,24 +1,30 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import sys
 
-df = pd.read_csv('triple-node-march-b.log', delimiter=r"\s+")
+if len(sys.argv) != 2:
+    print("Usage: {0} record.log".format(sys.argv[0]))
+else:
+    f_in = str(sys.argv[1])
+    f_out = f_in.replace('log', 'png')
 
-fig, axs = plt.subplots(nrows=1, ncols=2, sharey=True, figsize=(11, 3))
-fig.suptitle("BeeGFS Writing 100 MB Random Dataset", fontsize=14)
+    df = pd.read_csv(f_in, delimiter=r"\s+")
+    d_size = df.data.max() / 1000000
 
-ax = axs[0]
-ax.set_xlabel('avg file size / B')
-ax.set_xscale('log')
-ax.set_ylabel('time / s')
-# ax.errorbar(df.file, df.write, yerr=[df.stdev, df.stdev], color='blue', label='create')
-ax.plot(df.file, df.write, color='blue', label='create')
-ax.plot(df.file, df.remove, color='red', label='remove')
-ax.legend(loc='upper right', fontsize=10)
+    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(11, 3))
+    fig.suptitle("BeeGFS {0} MB Write Performance".format(d_size), fontsize=14)
 
-ax = axs[1]
-ax.set_xlabel('num files')
-ax.set_xscale('log')
-ax.plot(100000000 / df.file, df.write, color='blue', label='create')
-ax.plot(100000000 / df.file, df.remove, color='red', label='remove')
+    ax = axs[0]
+    ax.set_xlabel('avg file size / [B]')
+    ax.set_xscale('log')
+    ax.set_ylabel('time / [s]')
+    # ax.errorbar(df.file, df.write, yerr=[df.stdev, df.stdev], color='blue', label='create')
+    ax.plot(df.file, df.write, color='blue')
 
-fig.savefig('march-100M.png', bbox_inches='tight', dpi=400)
+    ax = axs[1]
+    ax.set_xlabel('avg file size / [B]')
+    ax.set_xscale('log')
+    ax.set_ylabel('rate / [MB/s]')
+    ax.plot(df.file, df.data / (1e6 * df.write), color='green')
+
+    fig.savefig(f_out, bbox_inches='tight', dpi=400)
